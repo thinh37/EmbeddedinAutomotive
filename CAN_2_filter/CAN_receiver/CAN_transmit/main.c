18bd07e5cbd20f59fa1f110ff36e9f23cad2388f
thinh37 <109522214+thinh37@users.noreply.github.com>
2023-08-01 23:28:49 +0700

    Add files via upload

---
+++ CAN_2_filter/CAN_receiver/CAN_transmit/main.c
@@ -0,0 +1,86 @@
+#include "stm32f10x.h"
+#include "stm32f10x_can.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "delay.h"
+void init_can(){
+    GPIO_InitTypeDef            GPIO_InitStructure;
+    CAN_InitTypeDef             CAN_InitStructure;
+    CAN_FilterInitTypeDef       CAN_FilterInitStructure;
+
+   
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1, ENABLE);
+	
+	
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	/* CAN cell init */
+    CAN_InitStructure.CAN_TTCM = DISABLE;
+    CAN_InitStructure.CAN_ABOM = DISABLE;
+    CAN_InitStructure.CAN_AWUM = DISABLE;
+    CAN_InitStructure.CAN_NART = DISABLE;
+    CAN_InitStructure.CAN_RFLM = DISABLE;
+    CAN_InitStructure.CAN_TXFP = DISABLE;
+    CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;
+  
+    /* CAN Baudrate = 1MBps*/
+    CAN_InitStructure.CAN_SJW = CAN_SJW_1tq;
+    CAN_InitStructure.CAN_BS1 = CAN_BS1_3tq;
+    CAN_InitStructure.CAN_BS2 = CAN_BS2_5tq;
+    CAN_InitStructure.CAN_Prescaler = 4;
+    CAN_Init(CAN1, &CAN_InitStructure);
+
+    CAN_FilterInitStructure.CAN_FilterNumber = 0;
+    CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;
+    CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
+    CAN_FilterInitStructure.CAN_FilterIdHigh = 0x0000;
+    CAN_FilterInitStructure.CAN_FilterIdLow = 0x0000;
+    CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0x0000;
+    CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
+    CAN_FilterInitStructure.CAN_FilterFIFOAssignment = CAN_FIFO0;
+    CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
+    CAN_FilterInit(&CAN_FilterInitStructure);
+}
+
+void send_Can_message(uint32_t id, uint8_t *data, uint8_t len){
+    CanTxMsg TxMessage;
+
+    TxMessage.StdId = id;
+    TxMessage.ExtId = 0x00;
+    TxMessage.RTR = CAN_RTR_DATA;
+    TxMessage.IDE = CAN_ID_STD;
+    TxMessage.DLC = len;
+
+    for (int i = 0; i < len; i++)
+    {
+        TxMessage.Data[i] = data[i];
+    }
+    CAN_Transmit(CAN1, &TxMessage);
+}
+int main(){
+		DelayInit();
+    init_can();
+    uint8_t data1[7]= {'h', 'e', 'l', 'l', 'o',' ',' '};
+		uint8_t data2[7]= {'x', 'i', 'n', 'c', 'h', 'a', 'o'};
+    while (1)
+    {
+			DelayMs(3000);
+      send_Can_message(0x123, (uint8_t *)"id 123 ", 7);
+			DelayMs(3000);
+      send_Can_message(0x456, (uint8_t *)"id 456 ", 7);
+			DelayMs(3000);
+       send_Can_message(0x123, data1, 7);
+      DelayMs(3000);
+			send_Can_message(0x456, data2, 7);
+    }
+}
